b4fd31bd349d17737cc42e56ca3c0a96c90c1ac5
trakjohnson <trakjohnsonweb@gmail.com>
2018-11-11 16:19:24 +0100

    Handle errors when loading file
    Works meh

---
+++ src/Error.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+
+export default class ErrorWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({hasError: true});
+    // You can also log the error to an error reporting service
+    // TODO maybe automatically get the faulty file ? (no need for an issue)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div id="error">
+        <div>
+          <h2>Zut alors, we encountered an error.</h2> <br/>
+          If you were trying to load a file, please report it by making an
+          <a href="https://github.com/Mapioi/OnlineLoggerPro/issues">issue here</a>
+          and including the problematic file.
+        </div>
+      </div>;
+    }
+
+    return this.props.children;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'dygraphs/dist/dygraph.min.css';
 import './index.css';
 import App from './App';
+import ErrorWrapper from './Error';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <ErrorWrapper>
+      <App/>
+    </ErrorWrapper>,
+    document.getElementById('root')
+);
 registerServiceWorker();
